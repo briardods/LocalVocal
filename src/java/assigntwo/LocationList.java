9a3da700b8ad0c60d346726c50d040364832728c
briar <beedods@gmail.com>
2016-05-26 11:12:09 +1200

    Added a new servlet to return a JSON Array with all locations.

---
+++ src/java/assigntwo/LocationList.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assigntwo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.naming.NamingException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class LocationList extends HttpServlet {
+
+    private PreparedStatement stmt;
+    private Connection conn;
+    private String dbDriver;
+    private String dbUrl;
+    private String dbTable;
+    private String userName;
+    private String password;
+
+    public LocationList()
+            throws SQLException, ClassNotFoundException, IOException, NamingException {
+        Properties properties = new Properties();
+        properties.loadFromXML(getClass().getResourceAsStream("VisitorServletConfig.xml"));
+        dbDriver = properties.get("dbDriver").toString();
+        dbUrl = properties.get("dbUrl").toString();
+        dbTable = properties.get("dbTable").toString();
+        userName = properties.get("user").toString();
+        password = properties.get("password").toString();
+
+        Class.forName(dbDriver);
+        conn = DriverManager.getConnection(dbUrl, userName, password);
+        stmt = conn.prepareStatement("SELECT * FROM " + dbTable);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        JSONObject json = new JSONObject();
+        JSONArray cities = new JSONArray();
+        try {
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                while (rs.next()) {
+                    json = new JSONObject();
+                    json.put("city", rs.getString("city"));
+                    json.put("latitude", rs.getBigDecimal("latitude"));
+                    json.put("longitude", rs.getBigDecimal("longitude"));
+                    cities.add(json);
+                }
+                System.out.println(json.toString());
+            } else {
+                json.put("info", "fail");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(cities.toString());
+    }
+}
